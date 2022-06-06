d1687f5eea68e96183d83933f2cae4e92165b543
Rhodanthe1116 <z0916945857@gmail.com>
2022-06-06 15:32:39 +0800

    downgrade

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 import { store } from './store/store'
 import $ from 'jquery'
 Vue.config.productionTip = true
 
-export const bus = new Vue();
+export const bus = new Vue()
 
 new Vue({
   store: store,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
 
 //關閉預設方向鍵滑動
-window.addEventListener("keydown", function (e) {
-  // space and arrow keys
-  if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-    e.preventDefault();
-  }
-}, false);
+window.addEventListener(
+  'keydown',
+  function (e) {
+    // space and arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault()
+    }
+  },
+  false
+)
 
-$('body')[0].style.zoom=0.75;
+$('body')[0].style.zoom = 0.75
