68b562807c0b8ba234470d7833c8d4dfaeb53282
Rhodanthe1116 <z0916945857@gmail.com>
2022-06-07 00:43:07 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 module.exports = {
   configureWebpack: {
@@ -10,4 +10,4 @@ module.exports = {
       }),
     ],
   },
-};
+}
